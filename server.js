25afd1646b8c0c45a780df181187f7270f1c5821
cjvrd <cjvirdo@gmail.com>
2023-08-16 12:25:43 +1000

    added database
    added database
    added database entries to form on page
    database links to cards on page

--- server.js
+++ server.js
@@ -1,29 +1,62 @@
 let express = require('express');
 let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.kn6c4iu.mongodb.net/?retryWrites=true&w=majority";
 let port = process.env.port || 3000;
+let collection;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res)=>{
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function runDBConnection() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log(collection);
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+
+app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-app.get('/addTwoNumbers', (req,res)=>{
-    // grab values from url parameter
-    // addTwoNumbers?number1=1&number2=2
-    let num1 = req.query.number1;
-    let num2 = req.query.number2;
-    
-    // do math
-    let sum = parseInt(num1) + parseInt(num2);
+app.get('/api/cats', (req, res) => {
+    getAllCats((err, result) => {
+        res.json({statusCode:200, data:result, message:'get all cats successful'});
+    });
+});
 
-    // construct response, include: statusCode, message, data
-    let obj = {statusCode:200, message:'success', data:sum};
-    
-    // then send back
-    res.json(obj);
-})
+app.post('/api/cat', (req, res) => {
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({statusCode:201, data:result, message:'success'});
+        }
+    });
+});
 
-app.listen(port, ()=>{
+function postCat(cat, callback) {
+    collection.insertOne(cat, callback);
+}
+
+function getAllCats(callback) {
+    collection.find({}).toArray(callback);
+}
+
+app.listen(port, () => {
     console.log('server started');
+    runDBConnection();
 });
